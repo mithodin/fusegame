6c7d10639c61a924e98c1e61f2e394373060a76f
Lucas L. Treffenstädt <lucas@treffenstaedt.de>
2018-05-20 10:15:23 +0200

    (very) basic game implemented

--- gamescript.py
+++ gamescript.py
@@ -1,8 +1,9 @@
 import utils
 
 def setup(game):
+    game.hl_create_file("/readme.txt","Welcome to the fuse game. Try creating a file called 'answer.txt' in the secret folder.\n",0o000)
     sf = game.hl_mkdir("/secret",0o755)
-    f = game.hl_create_file("/hallo.txt","Hallo Welt\n",0o600,True)
+    f = game.hl_create_file("/hello.txt","You've done it. This is all the game there is so far.\n",0o600,True)
     fa = utils.Node.get_node_available("/secret/answer.txt",game)
     action = f.get_show()
     cond = utils.Trigger.condition(fa,action)
